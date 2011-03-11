a703b3a7ff7fac842a6414f1f5d6953205ac200d
stephenmcd <steve@jupo.org>
2011-03-11 11:57:00 +1100

    Initial import.

---
+++ grappelli_safe/models/__init__.py
@@ -0,0 +1,3 @@
+from grappelli_safe.models.navigation import Navigation, NavigationItem
+from grappelli_safe.models.bookmarks import Bookmark, BookmarkItem
+from grappelli_safe.models.help import Help, HelpItem
---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="grappelli_safe",
+    version="0.1.5",
+    description="A snapshot of the grappelli_2 branch of django-grappelli, "
+                "packaged as a dependency for the Mezzanine CMS for Django.",
+    long_description=open("README.rst").read(),
+    author="Patrick Kranzlmueller, Axel Swoboda (vonautomatisch)",
+    author_email="werkstaetten@vonautomatisch.at",
+    maintainer="Stephen McDonald",
+    maintainer_email="stephen.mc@gmail.com",
+    url="http://github.com/stephenmcd/grappelli-safe/",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
+
