e85e6449a2fa04b2a60ddcee5e5086c9ff3ba75f
Stephen McDonald <steve@jupo.org>
2012-04-30 07:25:30 +1000

    Version bump to 0.2.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grappelli_safe",
-    version="0.2.5",
+    version="0.2.6",
     description="A snapshot of the grappelli_2 branch of django-grappelli, "
                 "packaged as a dependency for the Mezzanine CMS for Django.",
     long_description=open("README.rst").read(),
