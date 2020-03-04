94373ff1cf51ad3c1345edaf0632fbec2579616b
Peter Dermott <p.dermott@fermorltd.co.uk>
2020-03-04 16:35:48 +0000

    Update tested python versions

--- setup.py
+++ setup.py
@@ -18,9 +18,7 @@ setup(
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.8",
     ],
 )
